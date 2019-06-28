1b28cfc4b8426649a3d68696e3474e9a1e75fc20
jwkeena <jwkeena@gmail.com>
2019-06-28 12:19:05 -0400

    Update and delete routes added

--- controllers/cereal_controller.js
+++ controllers/cereal_controller.js
@@ -1,12 +1,15 @@
 const Cereal = require("../models/cereal.js");
 
 module.exports = function(app) {
+    
+    // Gets all bowls of cereal in database
     app.get("/api/all", function(req, res) {
         Cereal.findAll({}).then(function(results) {
             res.json(results);
         });
     });
     
+    // Adds a new bowl of cereal
     app.post("/api/new", function(req, res) {
         console.log("New bowl of cereal: "), req.body;
         Cereal.create({
@@ -16,4 +19,24 @@ module.exports = function(app) {
             res.end();
         });
     });
+
+    // Updates the given bowl of cereal as eaten
+    app.put("/api/:name", function(req, res) {
+        Cereal.update(
+            {eaten: true},
+            {where: {name: req.params.name}}
+        ).then(function(result) {
+            res.json(result);
+        })
+    });
+
+    // Deletes a bowl of cereal
+    app.delete("/api/:name", function(req, res) {
+        Cereal.destroy(
+            {where: {name: req.params.name}}
+        ).then( function(result) {
+            res.json(result);
+        })
+    })
+
 };
