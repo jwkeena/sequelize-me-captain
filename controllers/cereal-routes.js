a69bf68ff9d3ce6be160ec29848915a1e40b9cd0
jwkeena <jwkeena@gmail.com>
2019-07-05 16:38:15 -0400

    Beautified file

--- controllers/cereal-routes.js
+++ controllers/cereal-routes.js
@@ -1,53 +1,60 @@
 // Get all models in models directory
 const db = require("../models");
 
-module.exports = function(app) {
-    
+module.exports = function (app) {
+
     // Gets all bowls of cereal in database and displays them to the user
     app.get("/", function (req, res) {
-        db.Cereal.findAll({}).then(function(dbCereal) {
+        db.Cereal.findAll({}).then(function (dbCereal) {
 
             // console.log("This is the raw sequelize db response: ", dbCereal)
             let handlebarsObject = {
                 cereals: []
             }
 
-            for (let i=0; i<dbCereal.length; i++) {
+            for (let i = 0; i < dbCereal.length; i++) {
                 handlebarsObject.cereals.push(dbCereal[i].dataValues);
             }
-            
+
             // console.log("This is the handlebars object: ", handlebarsObject);
             res.render("index", handlebarsObject);
         })
     })
-    
+
     // Adds a new bowl of cereal
-    app.post("/api/new", function(req, res) {
+    app.post("/api/new", function (req, res) {
         console.log("New bowl of cereal added: ", req.body.name);
         db.Cereal.create({
             name: req.body.name,
             eaten: req.body.eaten,
-        }).then(function(dbCereal) {
+        }).then(function (dbCereal) {
             res.render("index", dbCereal);
         });
     });
 
     // Updates the given bowl of cereal as eaten
-    app.put("/api/:id", function(req, res) {
+    app.put("/api/:id", function (req, res) {
         db.Cereal.update(
             // Flipping the boolean here is apparently impossible. Must be done client side
-            {eaten: req.body.eaten},
-            {where: {id: req.params.id}}
-        ).then(function(dbCereal) {
+            {
+                eaten: req.body.eaten
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            }
+        ).then(function (dbCereal) {
             res.render("index", dbCereal);
         })
     });
 
     // Deletes a bowl of cereal
-    app.delete("/api/:id", function(req, res) {
-        db.Cereal.destroy(
-            {where: {id: req.params.id}}
-        ).then( function(dbCereal) {
+    app.delete("/api/:id", function (req, res) {
+        db.Cereal.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbCereal) {
             res.render("index", dbCereal);
         })
     })
